c06353fdcf0a17763e3d593aa75e2825f545d728
Jeremy Braun <23155614+jsbrn@users.noreply.github.com>
2020-12-14 20:58:35 -0600

    Working on new game mode, attempting to add clickable chat

--- src/main/java/commands/CreateGameInstanceCommand.java
+++ src/main/java/commands/CreateGameInstanceCommand.java
@@ -3,11 +3,16 @@ package commands;
 import com.onarandombox.MultiverseCore.MultiverseCore;
 import com.onarandombox.MultiverseCore.api.MVWorldManager;
 import games.RisingLavaGameInstance;
+import games.WaterDropGameInstance;
 import main.GameInstance;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import util.GameManager;
 
@@ -15,10 +20,21 @@ public class CreateGameInstanceCommand implements CommandExecutor {
 
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if (strings.length <= 0) return false;
-        GameInstance risingLava = new RisingLavaGameInstance();
+        GameInstance instance = null;
+        if (strings[0].equals("rising_lava")) instance = new RisingLavaGameInstance();
+        if (strings[0].equals("water_drop")) instance = new WaterDropGameInstance();
+        if (instance == null) {
+            commandSender.sendMessage(ChatColor.RED+"Sorry, that game mode does not exist!");
+            return true;
+        }
         commandSender.sendMessage("Creating new Rising Lava game...");
-        if (GameManager.createGameInstance(risingLava)) {
-            commandSender.sendMessage(ChatColor.GREEN+"Success! Do "+ChatColor.YELLOW+"/join "+risingLava.getName()+ChatColor.GREEN+" to reserve your spot!");
+        if (GameManager.createGameInstance(instance)) {
+            for (Player p: Bukkit.getOnlinePlayers()) {
+                TextComponent message = new TextComponent("A new game has started! Click here to join!");
+                //message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/joingame "+instance.getName()));
+                p.spigot().sendMessage(message);
+            }
+            commandSender.sendMessage(ChatColor.GREEN+"Success! Do "+ChatColor.YELLOW+"/join "+instance.getName()+ChatColor.GREEN+" to reserve your spot!");
         } else {
             commandSender.sendMessage(ChatColor.RED+"Failed!");
         }
---
+++ src/main/java/games/WaterDropGameInstance.java
@@ -0,0 +1,116 @@
+package games;
+
+import main.GameInstance;
+import main.GameManagerPlugin;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.*;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.awt.*;
+import java.util.Random;
+
+public class WaterDropGameInstance extends GameInstance {
+
+    private final Rectangle dropBounds;
+    private final int dropHeight, dropInterval, amountPerDrop;
+    private final Random random;
+
+    public WaterDropGameInstance() {
+        super("cod_arena", 5, 16);
+        this.dropHeight = 129;
+        this.dropInterval = 20;
+        this.amountPerDrop = 2;
+        this.random = new Random();
+        this.dropBounds = new Rectangle(-20, -20, 41, 41);
+    }
+
+    private BukkitRunnable dropRandomIngots = new BukkitRunnable() {
+        public void run() {
+            World w = getWorld();
+            int amount = 1 + random.nextInt(amountPerDrop);
+            for (int i = 0; i < amount; i++) {
+                Location l = new Location(
+                        w,
+                        dropBounds.x + random.nextInt(dropBounds.width),
+                        dropHeight,
+                        dropBounds.y + random.nextInt(dropBounds.height));
+                int distFromCenter = (int)l.distance(new Location(w, 0, dropHeight, 0));
+                w.dropItem(l, new ItemStack(random.nextInt(distFromCenter) == 0 ? Material.DIAMOND : Material.GOLD_NUGGET, random.nextInt(2)+1));
+            }
+        }
+    };
+
+    public void onStart() {
+        dropRandomIngots.runTaskTimer(GameManagerPlugin.getInstance(), 0, dropInterval);
+        for (Player p: getActivePlayers()) {
+            addRequiredItems(p);
+        }
+    }
+
+    private void addRequiredItems(Player p) {
+        ItemStack trident = new ItemStack(Material.TRIDENT, 1);
+        trident.addEnchantment(Enchantment.RIPTIDE, 3);
+        trident.addEnchantment(Enchantment.RIPTIDE, 3);
+        ItemStack boots = new ItemStack(Material.GOLDEN_BOOTS, 1);
+        boots.addEnchantment(Enchantment.DEPTH_STRIDER, 3);
+        boots.addEnchantment(Enchantment.SOUL_SPEED, 3);
+        p.getInventory().addItem(trident);
+        p.getInventory().setBoots(boots);
+    }
+
+    public boolean onNext() {
+        return false;
+    }
+
+    public void onStop() {
+        dropRandomIngots.cancel();
+    }
+
+    public void onFinish() {
+        dropRandomIngots.cancel();
+    }
+
+    @EventHandler
+    public void onChestClick(InventoryOpenEvent event) {
+        System.out.println("inv opened");
+        if (isFinished()) return;
+        if (event.getPlayer().getGameMode() != GameMode.SURVIVAL) return;
+        if (!event.getPlayer().getLocation().getWorld().equals(getWorld())) return;
+        System.out.println("passed requirements");
+        if (event.getInventory().getType().equals(InventoryType.CHEST)) {
+            System.out.println("is of type chest");
+            ItemStack[] items = event.getPlayer().getInventory().getContents();
+            System.out.println("there are "+items.length+" items");
+            for (ItemStack i: items)
+                if (i.getType().equals(Material.GOLD_NUGGET) || i.getType().equals(Material.DIAMOND)) event.getPlayer().getInventory().remove(i);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerTakesDamage(EntityDamageEvent event) {
+        if (event.getEntityType() != EntityType.PLAYER) return;
+        if (!event.getEntity().getLocation().getWorld().equals(getWorld())) return;
+        Player p = (Player)event.getEntity();
+        if (p.getHealth() - event.getFinalDamage() <= 0) {
+            event.setCancelled(true);
+            getWorld().playSound(p.getLocation(), Sound.ENTITY_PLAYER_DEATH, 1.0f, 1.0f);
+            getWorld().createExplosion(p.getLocation(), 1);
+            p.sendTitle(ChatColor.RED+"You died.", "", 0, 10, 5);
+            p.teleport(new Location(getWorld(), dropBounds.x + random.nextInt(dropBounds.width), 67, dropBounds.x + random.nextInt(dropBounds.width)));
+            p.getInventory().remove(Material.GOLD_NUGGET);
+            p.getInventory().remove(Material.DIAMOND);
+            p.setHealth(20);
+            addRequiredItems(p);
+        }
+    }
+
+}
